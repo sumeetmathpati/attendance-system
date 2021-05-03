bdd48527a4824c2d59261ab2393de327a71dfa4f
Sumeet Mathpati <sumeet.mathapti@gmail.com>
2021-05-03 15:15:08 +0530

    added date time picker

--- src/attendance_manager/forms.py
+++ src/attendance_manager/forms.py
@@ -1,5 +1,6 @@
-from django.forms import ModelChoiceField, EmailField, Form, DateField, DateTimeField
+from django.forms import ModelChoiceField, EmailField, Form, DateField, DateTimeField, DateTimeInput
 from .models import Class
+from datetime import datetime
 
 
 # class TakeAttendance(forms.Form):
@@ -9,11 +10,13 @@ from .models import Class
 #         model = Class
 #         fields = ['title']
 
+class MyDateInput(DateTimeInput):
+    input_type = 'datetime-local'
 
 class TakeAttendance(Form):
 
     # email = EmailField(readonly = True).attrs['readonly']
     class_name = ModelChoiceField(queryset=Class.objects.all(), to_field_name="title")
-    from_date = DateTimeField()
-    to_date = DateTimeField( ) 
+    from_date = DateTimeField(widget=MyDateInput)
+    to_date = DateTimeField(widget=MyDateInput) 
 
